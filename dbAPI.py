b477fc7e39b19bfb6b499832c68f0c561b6b0bf0
Tyler Krasny <tekrasny@gmail.om>
2021-04-09 09:24:12 -0400

    API Updates

--- dbAPI.py
+++ dbAPI.py
@@ -6,16 +6,20 @@ import pymysql
 import uvicorn
 from pydantic import BaseSettings
 
-get_user_query = "SELECT services FROM user_platforms WHERE acctID = ':acctID'"
+all_users_query = "Select username FROM user_info"
+get_services_query = "SELECT services FROM user_platforms WHERE acctID = ':acctID'"
 update_user_services = "UPDATE user_platforms SET services = ':services' WHERE acctID = ':acctID'"
 insert_user_sql = "INSERT INTO user_info (username, password, firstname, lastname, acctID) VALUES (%s, %s, %s, %s, %s)"
 find_max_acctID_sql = "SELECT MAX(acctID) as maxID FROM user_info"
+user_watchlist_query = "SELECT primaryTitle FROM watchlist WHERE acctID = '%s' AND removed <> '1'"
+user_watchlist_remove_show = "UPDATE watchlist SET removed = '1' WHERE primaryTitle = '%s' AND acctID = '%s'"
+user_watchlist_remove_all = "UPDATE watchlist SET removed = '1' WHERE acctID = '%s'"
 
 
 class Settings(BaseSettings):
-    sshuser: str = "<FILL ME IN>"
-    sshpass: str = "<FILL ME IN>"
-    dbpass: str = "<FILL ME IN>"
+    sshuser: str = "< >"
+    sshpass: str = "< >"
+    dbpass: str = "< >"
 
 
 settings = Settings()
@@ -30,7 +34,6 @@ def read_root():
 @app.get("/users")
 def read_all_users():
     returnList = []
-    query = "SHOW TABLES"
     with sshtunnel.SSHTunnelForwarder(
             ("129.74.154.187", 22),
             ssh_username=settings.sshuser,
@@ -46,7 +49,7 @@ def read_all_users():
         }
         connection = pymysql.connect(**config)
         cursor = connection.cursor()
-        cursor.execute(query)
+        cursor.execute(all_users_query)
         result = cursor.fetchall()
         for x in result:
             returnList.append(x)
@@ -55,7 +58,7 @@ def read_all_users():
     return returnList
 
 
-@app.put("/users/{user_id}")
+@app.put("/users/services/{user_id}")
 def update_services(user_id: str, service: str):
     with sshtunnel.SSHTunnelForwarder(
             ("129.74.154.187", 22),
@@ -74,7 +77,7 @@ def update_services(user_id: str, service: str):
         connection = pymysql.connect(**config)
         cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-        userQuery = get_user_query.replace(":acctID", user_id)
+        userQuery = get_services_query.replace(":acctID", user_id)
         cursor.execute(userQuery)
         result = cursor.fetchone()
 
@@ -122,7 +125,64 @@ def create_user(username: str, password: str, firstname: str, lastname: str):
         connection.commit()
         connection.close()
 
-        return {"user_id": 1}
+        return {"username": username, "password": password, "firstname": firstname, "lastname": lastname, "id": maxID+1}
+
+
+@app.get("/users/watchlist/{user_id}")
+def read_user_watchlist(user_id: str):
+    returnList = []
+    with sshtunnel.SSHTunnelForwarder(
+            ("129.74.154.187", 22),
+            ssh_username=settings.sshuser,
+            ssh_password=settings.sshpass,
+            remote_bind_address=("127.0.0.1", 3306)
+    ) as tunnel:
+        config = {
+            'user': "smanfred",
+            'password': settings.dbpass,
+            'host': "127.0.0.1",
+            'port': tunnel.local_bind_port,
+            'database': "smanfred"
+        }
+        connection = pymysql.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute(user_watchlist_query % user_id)
+        result = cursor.fetchall()
+        for x in result:
+            returnList.append(x)
+        connection.close()
+
+    return returnList
+
+@app.delete("/users/watchlist/{user_id}", status_code=200)
+def delete_watchlist(user_id: str, title: Optional[str] = None):
+    with sshtunnel.SSHTunnelForwarder(
+            ("129.74.154.187", 22),
+            ssh_username=settings.sshuser,
+            ssh_password=settings.sshpass,
+            remote_bind_address=("127.0.0.1", 3306)
+    ) as tunnel:
+        config = {
+            'user': "smanfred",
+            'password': settings.dbpass,
+            'host': "127.0.0.1",
+            'port': tunnel.local_bind_port,
+            'database': "smanfred"
+        }
+
+        connection = pymysql.connect(**config)
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+        if title:
+            # Delete just one show from watchlist when title is specified
+            cursor.execute(user_watchlist_remove_show % (title, user_id))
+
+        else:
+            # Clear entire watchlist when no title specified
+            cursor.execute(user_watchlist_remove_all % user_id)
+
+        connection.commit()
+        connection.close()
 
 
 if __name__ == "__main__":
